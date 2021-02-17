273b891aeb8dfd49893cdf50d83b13857c42e747
maoo <maoo@apache.org>
2021-02-17 17:52:06 +0100

    moved cert manager creation

--- installers/aws-eks/install/manage-eks.sh
+++ installers/aws-eks/install/manage-eks.sh
@@ -41,12 +41,15 @@ deploy_ingress_ctrl()
 {
         kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.44.0/deploy/static/provider/aws/deploy.yaml
 
+		kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.2.0/cert-manager.yaml
+
         kubectl apply -f $WORK_DIR/generated-ingress-config
 }
 
 delete_ingress_ctrl()
 {
 	kubectl delete -f $WORK_DIR/generated-ingress-config
+	kubectl delete -f https://github.com/jetstack/cert-manager/releases/download/v1.2.0/cert-manager.yaml
 	kubectl delete -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.44.0/deploy/static/provider/aws/deploy.yaml
 }
 
